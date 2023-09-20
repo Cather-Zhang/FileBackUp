bbe7cba1d0cce8be5191177062e8247630fbdbd5
milesgregg <milesgregg3@gmail.com>
2023-09-20 14:45:34 -0400

    makefile and test cases

---
+++ backup.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+
+void testAddition() {
+    int i = 5;
+    assert(i == 4);
+}
+
+int main() {
+    printf("Hello World\n");
+
+    testAddition();
+
+    return 0;
+}
