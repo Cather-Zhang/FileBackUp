c9b35a1b8895d19a956c68a8f51a4926d5dcc04a
Nathan Wong <93850704+nk-wong@users.noreply.github.com>
2023-09-20 15:03:45 -0400

    Added initial test directory

--- backup.c
+++ backup.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 void testAddition() {
     int i = 5;
-    assert(i == 4);
+    assert(i == 5);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return -1;
+    }
+
     printf("Hello World\n");
+
+    const char* sourceDirectory = (char*)malloc(strlen(argv[1]) + 1);
+    struct stat dirStat;
+    int result = stat(sourceDirectory, *dirStat);
+
+    if (result == 0) {
+        if (S_ISDIR(dirStat.st_mode)) {
+            printf("The directory '%s' exists.\n", sourceDirectory);
+            return 0;
+        } else {
+            printf("'%s' is not a directory.\n", sourceDirectory);
+            return -1;
+        }
+    }
+    else {
+        printf("%s is not a directory", sourceDirectory);
+        return -1;
+    }
+
 
     testAddition();
 
