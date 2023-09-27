5876b4c9316b544a3958551434316e5428754721
Cather Zhang <cather0215@gmail.com>
2023-09-26 20:22:18 -0400

    Add display of all files in source folder

--- backup.c
+++ backup.c
@@ -5,7 +5,6 @@
 #include <stdlib.h>
 #include <dirent.h>
 
-#define PATH_MAX 256
 
 void checkNewPath(char* path) {
     int result = strcmp(path, "dummy_files_backup");
@@ -14,10 +13,11 @@ void checkNewPath(char* path) {
 
 void listFilesAndDirectories(char* sourcePath) {
     struct dirent *dp;
+    const char* pathSlash = "/";
 
     DIR *sourceDirectory = opendir(sourcePath);
     if (!sourceDirectory) {
-        printf("Source Directory Doesn't Exist\n");
+        printf("Source Directory %s Doesn't Exist\n", sourcePath);
         return;
     }
     
@@ -25,13 +25,14 @@ void listFilesAndDirectories(char* sourcePath) {
         //skipping . and .. directories
         if ((strcmp(dp->d_name, ".") != 0) && (strcmp(dp->d_name, "..") != 0)) {
             printf("%s/%s\n", sourcePath, dp->d_name);
+            if (dp->d_type == DT_DIR) {
+                char* newPath = strndup(sourcePath, strlen(sourcePath));
+                strcat(newPath, pathSlash);
+                strcat(newPath, dp->d_name);
+                listFilesAndDirectories(newPath);
+            }
         }
 
-        if (dp->d_type == DT_DIR) {
-            char path[PATH_MAX];
-            snprintf(path, sizeof(path), "%s/%s", sourcePath, dp->d_name);
-            listFilesAndDirectories(path);
-        }
     }
 
 
