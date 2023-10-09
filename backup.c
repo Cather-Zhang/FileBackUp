bb97ffb0bf70f0b21e7369d4c29db04c97f0efd5
Cather Zhang <cather0215@gmail.com>
2023-10-09 03:09:45 -0400

    Add file and folder stats copy, debug needed

--- backup.c
+++ backup.c
@@ -5,85 +5,75 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <utime.h>
+#include <pwd.h>
+#include <grp.h>
 
 #include "tester.h"
 
 
-void checkNewPath(char* path) {
-    int result = strcmp(path, "dummy_files_backup");
-    assert(result == 0);
-}
-
-void copyFilesAndDirectories(char* sourcePath, char* targetPath) {
+int copyFilesAndDirectories(char* sourcePath, char* targetPath) {
     struct dirent *dp;
     const char* pathSlash = "/";
     char newSourcePath[256];
     char newTargetPath[256];
-    printf("Input: %s and %s\n", sourcePath, targetPath);
 
     DIR *sourceDirectory = opendir(sourcePath);
     if (!sourceDirectory) {
         printf("ERROR: Source Directory %s Doesn't Exist\n", sourcePath);
-        return;
+        return 1;
     }
-    
-    struct stat info;
 
-    if (stat(targetPath, &info) != 0) {
-        printf("Target Directory %s Doesn't Exist, creating one now\n", targetPath);
-        mkdir(targetPath, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-        // Try to open the target dir again
-        
-    }
     DIR* targetDirectory = opendir(targetPath);
     if (!targetDirectory) {
-        printf("ERROR: Target Directory %s Doesn't Exist\n", targetPath);
-        return;
+        perror("Unable to open target directory");
+        return 1;
     }
-    
-    
+
     while ((dp = readdir(sourceDirectory)) != NULL) {
         //skipping . and .. directories
         if ((strcmp(dp->d_name, ".") == 0) || (strcmp(dp->d_name, "..") == 0)) {
             continue;
         }
 
-        printf("%s/%s\n", sourcePath, dp->d_name);
+        //printf("%s/%s\n", sourcePath, dp->d_name);
 
-/*
-        char* newSourcePath = strndup(sourcePath, strlen(sourcePath)+1);
-        strcat(newSourcePath, pathSlash);
-        strcat(newSourcePath, dp->d_name);
-
-        char* newTargetPath = strndup(targetPath, strlen(targetPath)+1);
-        strcat(newTargetPath, pathSlash);
-        strcat(newTargetPath, dp->d_name);
-*/
         snprintf(newSourcePath, sizeof(newSourcePath), "%s/%s", sourcePath, dp->d_name);
         snprintf(newTargetPath, sizeof(newTargetPath), "%s/%s", targetPath, dp->d_name);
-        printf("NEW PATHS: %s to %s\n", newSourcePath, newTargetPath);
+        //printf("NEW PATHS: %s to %s\n", newSourcePath, newTargetPath);
 
         // If it is a folder, we recursively copy sub-directories
         if (dp->d_type == DT_DIR) {
-            printf("%s is a directory, recursively calling the function\n", newSourcePath);
+            //printf("%s is a directory, recursively calling the function\n", newSourcePath);
+            copyFolderStats(newSourcePath, newTargetPath);
             copyFilesAndDirectories(newSourcePath, newTargetPath);
         }
         else {
-            printf("It is a file\n");
+            //printf("It is a file\n");
             FILE* sourceFile = fopen(newSourcePath, "rb");
             if (sourceFile == NULL) {
                 printf("Error opening source file to be copied!\n");
+                return 1;
                 //continue;
             }
-            
+
+            // Get the source file's stats
+            struct stat sourceStat;
+            if (stat(newSourcePath, &sourceStat) == -1) {
+                perror("Failed to get source file stats");
+                fclose(sourceFile);
+                return 1;
+            }
+
             FILE* targetFile = fopen(newTargetPath, "wb");
             
             if (targetFile == NULL) {
                 printf("Error opening or creating target file\n");
+                return 1;
                 //continue;
             }
 
-            printf("copying");
+            //printf("copying");
             char buffer[4096];
             size_t bytes_read;
             while ((bytes_read = fread(buffer, 1, sizeof(buffer), sourceFile)) > 0) {
@@ -91,7 +81,11 @@ void copyFilesAndDirectories(char* sourcePath, char* targetPath) {
                 //printf(".");
             }
 
-            printf("File %s copied successfully\n", newSourcePath);
+            //printf("File %s copied successfully\n", newSourcePath);
+
+
+            // Copy file ownership and permission
+            //copyFileStats(newSourcePath, newTargetPath);
 
             fclose(sourceFile);
             fclose(targetFile);
@@ -101,6 +95,59 @@ void copyFilesAndDirectories(char* sourcePath, char* targetPath) {
     closedir(sourceDirectory);
     closedir(targetDirectory);
 
+    return 0;
+}
+
+int copyFileStats(char* source, char* target) {
+    if (chown(newTargetPath, sourceStat.st_uid, sourceStat.st_gid) == -1) {
+        perror("Failed to copy file ownership");
+        return 1;
+    }
+    if (fchmod(targetFile, sourceStat.st_mode) == -1) {
+        perror("Failed to copy file permissions");
+        return 1;
+    }
+
+    // Copy file timestamps
+    struct utimbuf utime_buf;
+    utime_buf.actime = sourceStat.st_atime;
+    utime_buf.modtime = sourceStat.st_mtime;
+    if (utime(targetFile, &utime_buf) == -1) {
+        perror("Failed to copy file timestamps");
+        return 1;
+    }
+    return 0;
+}
+
+int copyFolderStats(char* source, char* target) {
+    struct stat sourceStat;
+    struct utimbuf utimeBuf;
+
+    if (stat(source, &sourceStat) == -1) {
+        perror("Error getting folder stats");
+        return -1; // Handle the error as needed
+    }
+
+    // Create the destination folder with the same permissions as the source folder
+    if (mkdir(target, sourceStat.st_mode) == -1) {
+        perror("Error creating destination folder");
+        return -1; // Handle the error as needed
+    }
+
+    // Set ownership of the destination folder to match the source folder
+    if (chown(target, sourceStat.st_uid, sourceStat.st_gid) == -1) {
+        perror("Error setting folder ownership");
+        return -1; // Handle the error as needed
+    }
+
+    // Set timestamps (atime and mtime) of the destination folder to match the source folder
+    utimeBuf.actime = sourceStat.st_atime;
+    utimeBuf.modtime = sourceStat.st_mtime;
+    if (utime(target, &utimeBuf) == -1) {
+        perror("Error setting folder timestamps");
+        return -1; // Handle the error as needed
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -112,10 +159,7 @@ int main(int argc, char *argv[]) {
     }
     const char* target_suffix = "_backup";
     char* sourceDirectoryName = argv[1];
-    //char* targetDirectoryName = malloc((strlen(sourceDirectoryName)+1)*sizeof(char));
-    //strcpy(targetDirectoryName, sourceDirectoryName);
-    //strcat(targetDirectoryName, target_suffix);
-    
+
     char* targetDirectoryName = strndup(sourceDirectoryName, strlen(sourceDirectoryName)+1);
     strcat(targetDirectoryName, target_suffix);
 
@@ -125,7 +169,16 @@ int main(int argc, char *argv[]) {
     printf("Source directory: %s\n", sourceDirectoryName);
     printf("Target directory: %s\n", targetDirectoryName);
 
-    copyFilesAndDirectories(sourceDirectoryName, targetDirectoryName);
+    // Create target folder first
+    if (copyFolderStats(sourceDirectoryName, targetDirectoryName) != 0) {
+        perror("Unable to create backup folder with same stats as source!");
+        return 1;
+    }
+
+    if (copyFilesAndDirectories(sourceDirectoryName, targetDirectoryName) != 0) {
+        perror("Unable to backup folder!");
+        return 1;
+    }
 
     return 0;
 }
--- tester.c
+++ tester.c
@@ -2,6 +2,29 @@
 #include <stdio.h>
 #include <assert.h>
 #include <unistd.h>
+#include <sys/stat.h>
+
+int compareStats(char *source, char *target) {
+    struct stat source_stat, target_stat;
+
+    if (stat(source, &source_stat) == -1 || stat(target, &target_stat) == -1) {
+        perror("Error getting file stats");
+        return -1; // Handle the error as needed
+    }
+
+    // Compare the relevant attributes (e.g., size, modification time, etc.)
+    if (source_stat.st_size != target_stat.st_size) {
+        return 0; // Sizes are different
+    }
+
+    if (source_stat.st_mtime != target_stat.st_mtime) {
+        return 0; // Modification times are different
+    }
+
+    // Add more comparisons for other attributes as needed
+
+    return 1; // Stats are the same
+}
 
 void test_copyFileToDirectory() {
     char* sourcePath = "source_directory";
@@ -16,6 +39,10 @@ void test_copyFileToDirectory() {
 
     // Check if the file was copied to the target directory
     assert(access("target_directory/source_file.txt", F_OK) != -1);
+
+    // Make sure all stats are copied over as well
+    //assert(compareStats(sourcePath, targetPath) == 1);
+    //assert(compareStats("source_directory/source_file.txt", "target_directory/source_file.txt") == 1);
 
     // Clean up
     remove("target_directory/source_file.txt");
@@ -54,6 +81,11 @@ void test_copyNestedDirectories() {
     // Check if the directories and files were copied correctly
     assert(access("target_directory/subdir1", F_OK) != -1);
     assert(access("target_directory/subdir1/file1.txt", F_OK) != -1);
+
+    // Make sure all stats are copied over as well
+    //assert(compareStats(sourcePath, targetPath) == 1);
+    //assert(compareStats("nested_source_directory/subdir1", "target_directory/subdir1") == 1);
+    //assert(compareStats("nested_source_directory/subdir1/file1.txt", "target_directory/subdir1/file1.txt") == 1);
 
     // Clean up
     remove("nested_source_directory/subdir1/file1.txt");
